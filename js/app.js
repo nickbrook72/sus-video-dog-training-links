b4ec29e3b3e0fe02c1b52e86159836c1e9765d1f
Nick <nick@crowdsurfer.com>
2014-12-13 14:22:58 +0000

    remove .idea files added href links

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
 
     $('#video-links-table').DataTable({
-        data: VideoData
+        data: VideoData,
+        columnDefs: [
+            {
+                render: function(data) {
+                    return '<a target="_blank" href="'+data+'">'+data+'</a>';
+                },
+                targets: 3
+            }
+        ]
     })
 });
